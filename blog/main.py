166198cd2836107505b2cce568834f4dc88519d7
River <riveriswild@icloud.com>
2023-01-01 20:18:44 +0300

    Add db

---
+++ blog/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from . import schemas
+
+
+app = FastAPI()
+
+
+@app.post('/blog')
+def create(request: schemas.Blog):
+    return request
+
+
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 
 app = FastAPI()
@@ -6,3 +9,19 @@ app = FastAPI()
 @app.get('/')
 def index():
     return ""
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+    
+
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return {'data': f'Blog is created with title {blog.title}'}
+
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
