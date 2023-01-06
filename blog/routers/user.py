15e3bb7a3ab9a5da402aa1bfc31da23cd2905fa6
River <riveriswild@icloud.com>
2023-01-06 21:46:12 +0300

    Refactoring

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -14,9 +14,9 @@ get_db = database.get_db
 
 @router.post('/', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    user.create(request, db)
+    return user.create(request, db)
     
     
 @router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id:int, db: Session = Depends(get_db)):
-    user.show(id, db)
+    return user.show(id, db)
