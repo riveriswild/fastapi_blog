56f010086b245763cd4cb4d99e9327d5581b19ed
River <riveriswild@icloud.com>
2023-01-06 21:37:35 +0300

    Refactoring

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -2,6 +2,7 @@ from typing import List
 from fastapi import APIRouter, Depends, status, Response, HTTPException
 from .. import schemas, database, models
 from sqlalchemy.orm import Session 
+from ..repository import blog
 
 
 router = APIRouter(
@@ -13,38 +14,21 @@ get_db = database.get_db
 
 @router.get('/', response_model = List[schemas.ShowBlog])
 def all(db: Session = Depends(database.get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    return 'done'
+    return(blog.destroy(id, db))
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with id {id} not found")
-    blog.update(request)
-    db.commit()
-    return 'done'
+    return blog.update(id, request, db)
 
 
 @router.get('/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with id {id} Not Found')
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'details': f'blog with id {id} Not Found'}
-    return blog
+    return blog.show(id, db)
